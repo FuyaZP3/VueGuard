644a3c6b71bf4fbc293cf54e327eb4bf13f49309
FuyaZP3 <fuyazp3@gmail.com>
2026-01-21 00:59:26 +0300

    feat: add settings for apiKey & check user apiKey

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,29 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('vue-guard is now active!');
+	console.log('VueGuard active!');
 
 	const hoverProvider = vscode.languages.registerHoverProvider('vue', {
 		provideHover(document, position) {
+			// Önce kullanıcının apiKey tanımlamasını kontrol ediyoruz
+			const config = vscode.workspace.getConfiguration('vueGuard');
+			const apiKey = config.get<string>('apiKey');
+
+			if(!apiKey) {
+				return new vscode.Hover(
+					new vscode.MarkdownString('⚠️ **VueGuard:** Lütfen ayarlardan bir API Key tanımlayın.')
+				);
+			}
+
+			// Seçilen aralığı ve kelimeyi alıyoruz
 			const range = document.getWordRangeAtPosition(position);
 			const word = document.getText(range);
 
 			if(word) {
 				return new vscode.Hover(
-					new vscode.MarkdownString(`### 🛡️ VueGuard Test\n\nŞu an **${word}** üzerindesiniz.`)
+					new vscode.MarkdownString(`🛡️ **VueGuard Modu:** Aktif\n\n**API Key:** ${apiKey}\n\n**Kelime:** ${word}`)
 				);
 			}
-
-			// // return {
-			// // 	contents: ['Hover Content']
-			// // };
 		}
 	});
 
